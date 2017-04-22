b2dbc0528d92cf57a340908d79df1c0473a2e1c6
Fabrizio Bertone <fab.bertone@gmail.com>
2017-04-22 13:57:54 +0200

    implementation

--- README.md
+++ README.md
@@ -3,15 +3,12 @@
 ## User stories:
 
 1. I can get the image URLs, alt text and page urls for a set of images relating to a given search string.
-
 2. I can paginate through the responses by adding a ?offset=2 parameter to the URL.
-
 3. I can get a list of the most recently submitted search strings.
 
 ### Example usage:
 
 * http://localhost:5000/api/imagesearch/lolcats%20funny?offset=10
-
 * http://localhost:5000/api/latest/imagesearch/
 
 ## Live preview
@@ -19,9 +16,30 @@
 Coming Soon
 
 ## Info
+
 Image Search Abstraction Layer API project for [Free Code Camp] Back End Certification.
 
-###License
+### Configuration
+
+* Register on Google Custom search engine
+* Create a new **API key**
+* Create a new Custom search engine and get the **ID (cx)**
+* Set the API key in **CSE_API_KEY** and ID in **GOOGLE_CX** environment variables
+* Start the server with npm start (after installing all dependencies with npm install)
+
+Example:
+
+```
+CSE_API_KEY=<YOUR_API_KEY> GOOGLE_CX=<YOUR_CSE_ID> npm start
+```
+
+Additional settings (as env variables):
+
+* PORT: the service port
+* MAX_HISTORY: number of queries to keep in history (0 = no limit)
+* RESULTS: number of results to return
+
+### License
 
 **MIT**
 
--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-console.log('HELLO WORLD')
+// you have to get an API key from Google Custom Search Engine and set it in CSE_API_KEY envivorment variable
+// and also configure a search engine and put its ID in GOOGLE_CX
+if (!process.env.CSE_API_KEY || !process.env.CSE_API_KEY.length) {
+  console.error('ERROR: missing Google Custom Search Engine API Key in process.env.CSE_API_KEY')
+  process.exit(1)
+} else if (!process.env.GOOGLE_CX || !process.env.GOOGLE_CX.length) {
+  console.error('ERROR: missing Google Custom search engine ID (cx) in process.env.GOOGLE_CX')
+  process.exit(1)
+} else {
+  var apiKey = process.env.CSE_API_KEY
+  var cx = process.env.GOOGLE_CX
+  var express = require('express')
+  var request = require('request')
+  var app = express()
+  var port = process.env.PORT || 5000
+  // set max number of queries to keep in history (0 = no limit)
+  var maxHistory = process.env.MAX_HISTORY || 10
+  var results = process.env.RESULTS || 10
+  var history = []
+
+  app.get('/api/imagesearch/:query', function (req, res) {
+    var term = req.params.query
+    var offset = req.query.offset || 0
+    history.unshift({term: term, when: new Date()})
+    if (maxHistory > 0 && history.length > maxHistory) {
+      history.pop()
+    }
+    var pagination = offset * results + 1
+    var url = 'https://www.googleapis.com/customsearch/v1?q=' + term + '&cx=' + cx + '&searchType=image&fields=items(image(contextLink%2CthumbnailLink)%2Clink%2Ctitle)&num=' + results + '&start=' + pagination + '&key=' + apiKey
+
+    request(url, function (error, response, body) {
+      if (error) {
+        // console.log('error: ', error)
+        res.json({error: error})
+      } else if (response && response.statusCode !== 200) {
+        // console.log('statusCode != 200: ', response.statusCode, body)
+        res.json(JSON.parse(body))
+      } else {
+        // console.log(JSON.parse(body))
+        // TODO: yeah, I should handle JSON parsing error
+        var got = JSON.parse(body)
+        if (got.items && got.items.length > 0) {
+          var imgs = got.items.map(function (elem) {
+            return {alt: elem.title, url: elem.link, page: elem.image.contextLink, thumbnail: elem.image.thumbnailLink}
+          })
+          res.json(imgs)
+        } else {
+          res.json(got)
+        }
+      }
+    })
+  })
+
+  app.get('/api/latest/imagesearch/', function (req, res) {
+    res.json(history)
+  })
+
+  app.listen(port, function () {
+    console.log('Image Search Abstraction Layer started on port ' + port + '!')
+  })
+}
